a8cc4d99f001e2b96ecf03f5dcb9fe8b60827d4b
JohnDoeCat <JohnDoeCat@qq.com>
2021-05-16 17:52:12 +0800

    单元测试

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router'
+import store from './store'
+import ElementPlus from 'element-plus'
+
+// 全局引入样式
+import 'element-plus/lib/theme-chalk/index.css'
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(ElementPlus)
+  .mount('#app')
