0e2c23b5bbfa503d8cf3f9f32dd22c28ae2b04a1
JohnDoeCat <JohnDoeCat@qq.com>
2021-05-17 22:54:58 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,12 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
-//
-// import router from './router'
-// import { store } from "./store"
-// // import ElementPlus from 'element-plus'
-//
-// // 全局引入样式
-// import 'element-plus/lib/theme-chalk/index.css'
-// createApp(App)
-//   .use(router)
-//   .use(store)
-//   // .use(ElementPlus)
-//   .mount('#app')
 import { createApp } from 'vue'
-import importUiFramework from '@/utils/import-ui-framework'
-import router from '@/router/index'
+import router from '@/router'
 import { key, store } from '@/store'
+
+import { ElIcon, ElLoading, ElCard, ElButton } from 'element-plus'
 import App from './App.vue'
+
+// 全局引入样式
+import 'element-plus/lib/theme-chalk/index.css'
 import '@/style/basic.styl'
 
-const app = createApp(App)
-importUiFramework(app).use(router).use(store, key).mount('#app')
+createApp(App).use(router).use(store, key).use(ElIcon, ElLoading, ElCard, ElButton).mount('#app')
